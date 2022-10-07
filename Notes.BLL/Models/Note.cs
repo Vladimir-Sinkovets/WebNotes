ada3e46f470d9f3d76dd30c26b20f586f25db044
vladimir-sinkovets <vovasin49@gmail.com>
2022-10-07 15:38:52 +0300

    1

--- Notes.BLL/Models/Note.cs
+++ Notes.BLL/Models/Note.cs
@@ -8,6 +8,7 @@ namespace Notes.BLL.Models
 {
     public class Note
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
     }

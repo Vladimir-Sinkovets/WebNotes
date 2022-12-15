551e02598961cf13c24608134b076c0fc346d63f
vladimir-sinkovets <vovasin49@gmail.com>
2022-12-15 14:45:34 +0300

    1

--- Notes.BLL/Services/NoteManagers/Models/SearchFilter.cs
+++ Notes.BLL/Services/NoteManagers/Models/SearchFilter.cs
@@ -9,12 +9,21 @@ namespace Notes.BLL.Services.NoteManagers.Models
 {
     public class SearchFilter
     {
+        public SearchFilter()
+        {
+            Tags = new List<string>();
+
+            UseMaxLength = false;
+
+            UseMinLength = false;
+        }
+
         public string Title { get; set; }
         public string Text { get; set; }
-        public IEnumerable<string> Tags { get; set; }
+        public IEnumerable<string> Tags { get; set; } 
 
-        public bool UseMinLength { get; set; } = false;
-        public bool UseMaxLength { get; set; } = false;
+        public bool UseMinLength { get; set; }
+        public bool UseMaxLength { get; set; }
 
         public int MinLength { get; set; }
         public int MaxLength { get; set; }

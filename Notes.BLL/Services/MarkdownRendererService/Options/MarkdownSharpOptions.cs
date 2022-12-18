a2de98dc052eceed26413b25da7382081693de0d
vladimir-sinkovets <vovasin49@gmail.com>
2022-12-18 19:17:21 +0300

    add option

---
+++ Notes.BLL/Services/MarkdownRendererService/Options/MarkdownSharpOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Notes.BLL.Services.MarkdownRendererService.Options
+{
+    public class MarkdownSharpOptions
+    {
+        public bool AllowTargetBlank { get; set; }
+        public bool AllowEmptyLinkText { get; set; } = true;
+        public bool DisableImages { get; set; }
+        public bool QuoteSingleLine { get; set; }
+        public bool AutoHyperlink { get; set; }
+        public bool AutoNewLines { get; set; }
+        public bool LinkEmails { get; set; } = true;
+        public bool StrictBoldItalic { get; set; }
+        public bool AsteriskIntraWordEmphasis { get; set; }
+        public bool LinkEmailsWithoutAngleBrackets { get; set; }
+    }
+}
